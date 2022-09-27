2e67d4c9e9f2ae989c089a4e24d5792d14ea6c77
Mischa Michael <louism@vt.edu>
2022-09-27 10:24:58 -0700

    working version 1

--- MakeDiget.py
+++ MakeDiget.py
@@ -1,29 +1,66 @@
 #!/usr/bin/env python
 from PIL import Image, ImageFont, ImageDraw
 import argparse
-
+import random
 # Create 300 pixel per inch PNG for the numbers 1 - 75 in the front specified by the user
-def main(fontFile):
+def main(fontFile, fontColor, templateFile, numberOfCards, cardDestination):
+    for cardNumber in range(numberOfCards):
+        values = []
+        for i in range(5):
+            values.append([])
+            for j in range(5):
+                # print(f"i: {i} j:{j}")
+                candidate = random.randint((i*15)+1,(i*15)+15)
+                # print(f"candidate first roll: {candidate}")
+
+                while(candidate in values[i]):
+                    candidate = random.randint((i*15)+1,(i*15)+15)
+                    # print(f"candidate reRoll: {candidate}")
+                values[i].append(candidate)
+                # print(f"candidate used: {candidate}")
+
+        # one square will be .6 in x .6 in at 300 pixels per inch this is 180 x 180
+        W, H = (180*5,180*5)
+        im = Image.new("RGBA",(W,H),None)
+        draw = ImageDraw.Draw(im)
+        font = ImageFont.truetype(fontFile, 50)
+
+    
+
+        # draw.textbbox((0,0), msg, font=font, anchor=None, spacing=4, align='left', direction=None, features=None, language=None, stroke_width=0, embedded_color=False)
+        # future perfect vertical alignment could be achieved with textbbox sizeing
+        # w, h = draw.textsize(msg)
+        # print(f"width of text: {w} height of text: {h}")
+
+        for i in range(5):
+            for j in range(5):
+                draw.text((90 + (i*180),90+(j*180)), f"{values[i][j]}", fill=fontColor, anchor="mm", font=font)
+
+        # lines from top to bottom
+        for i in range(4):
+            draw.line(((180*(i+1), 0), (180*(i+1), 180*5)), fontColor)
+        for i in range(4):
+            draw.line(((0, 180*(i+1)), (180*5, 180*(i+1))), fontColor)
+        background = Image.open(templateFile)
+        foreground = im
+
+        # .5 in x 1.5 in offset, see layout image for visual
+        background.paste(foreground, (150, 450), foreground)
+        background.save(f"{cardDestination}/{cardNumber}.png", "PNG")
 
 
-    W, H = (180,180)
-    msg = "1"
-
-    im = Image.new("RGBA",(W,H),"white")
-    draw = ImageDraw.Draw(im)
-    draw.textbbox((0,0), msg, font=font, anchor=None, spacing=4, align='left', direction=None, features=None, language=None, stroke_width=0, embedded_color=False)
-    w, h = draw.textsize(msg)
-    print(f"width of text: {w} height of text: {h}")
-    font = ImageFont.truetype("arial.ttf", 10)
-
-    draw.text(((W-w)/2,(H-h)/2), msg, fill="black", font=font)
-    im.save("hello.png", "PNG")
-
-
-parser = argparse.ArgumentParser(description='given a font output PNG of the numbers 1-75 in this font')
+parser = argparse.ArgumentParser(description='given a font output PNG of a grid of bingo numbers')
 parser.add_argument('--font-file', help='path to a font file', required=False,
-  dest='fontFile')
+  dest='fontFile', default='arial.ttf')
+parser.add_argument('--font-color', help='font color', required=False,
+  dest='fontColor', default='black')
+parser.add_argument('--template-file', help='the path to a 1200px X 1500px png file that will be used as the template for the cards', required=False,
+  dest='templateFile', default='./Images/exampleTemplate.png')
+parser.add_argument('--number-of-cards', type=int, help='the number of cards you want to make', required=False,
+  dest='numberOfCards', default=1)
+parser.add_argument('--card-destination', help='the file path to put cards', required=False,
+  dest='cardDestination', default='./CardsOut')
 
 args = parser.parse_args()
 # Here we go!
-main(args.fontFile)
+main(args.fontFile, args.fontColor, args.templateFile, args.numberOfCards, args.cardDestination)
--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Process Idea
-- make images of each set of numbers, 1 - 75
-    - pick a font
-        - Font import from PIL in Python: 
-        `PIL.ImageFont.truetype(font=None, size=10, index=0, encoding='', layout_engine=None)`
-        https://pillow.readthedocs.io/en/stable/reference/ImageFont.html
 
 - Generate a set of numbers
     - 5 in each letter based on the below rules
         - random number between 1 & 15 if already used roll again
         - repeat with an offset for each set
+- layout the gid of numbers, text anchors allow for accurate layout: `d.text((100, 100), "Quick", fill="black", anchor="ms", font=font)`
+ref: https://pillow.readthedocs.io/en/stable/handbook/text-anchors.html#text-anchors
+    - pick a font
+        - Font import from PIL in Python: 
+        `PIL.ImageFont.truetype(font=None, size=10, index=0, encoding='', layout_engine=None)`
+        https://pillow.readthedocs.io/en/stable/reference/ImageFont.html
 
 - Do some math to get how big each size each number is as a picture
 - Put that pictures into a board so the center of the picture is the center of the grid space
@@ -18,7 +19,7 @@
 
 
 # Usage
-
+py .\MakeDiget.py --font-file bahnschrift.ttf --font-color red --number-of-cards 50
 
 # Bingo Number Rules
 Each space in the 'B' column contains a number from 1 - 15.
